b310a214d67d1de0ac18a3a7f6f5e03048601ba5
Jueon Park <lark1043@naver.com>
2018-03-26 05:51:07 +0000

    test files

---
+++ src/nodejs/basicserver.js
@@ -0,0 +1,10 @@
+var http = require('http');
+
+var server = http.createServer(function(req, res){
+    console.log('request was made: ' + req.url);
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hey Now Now');
+});
+
+server.listen(3000);
+console.log('yo dawgs, now listening to port 3000');
